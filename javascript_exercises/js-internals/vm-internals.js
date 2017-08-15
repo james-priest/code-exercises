d2d745346cada6f40aa6d72b9e510c61002a28c9
James Priest <james-priest@outlook.com>
2017-08-14 22:45:27 -0700

    Update vm-internals.js

--- javascript_exercises/js-internals/vm-internals.js
+++ javascript_exercises/js-internals/vm-internals.js
@@ -1,20 +1,51 @@
-var a = 2;              // Global Scope (Window)
-b = 2;                  // a    2
-                        // f    ---------> Lambda "f"
-function f(z) {         // b    1 (created at runtime)
-    b = 3;              // ---------------------------
-    c = 4;
-    var d = 6;          // Local execution scope for f()
-    e = 1;              // z
-                        // d
-    function g() {      // g    ---------> Lambda "g"
-        var e = 0;      // e      (line 17 - variable hosting)
-        d = 3*d;        // ---------------------------
-        return d;
+/** 
+ * JavaScript VM Internals
+ * http://jstherightway.org/#must-see
+ */
+/*
+// v1
+var a = 2;             // Global Scope (Window)
+b = 1;                 // a | a=2    |   |
+                       // f | f="λf" |---|------> Lambda "f"
+function f(z) {        //   | b=1    |   | b=3    ('b' created at runtime)
+    b = 3;             //   |        |   | c=4
+    c = 4;             // ------------------------
+    var d = 6;
+    e = 1;             // Local execution scope for f()  (garbage collected)
+                       // z | z=1    |   |
+    function g() {     // d | d=6    |   | d=18
+        var e = 0;     // g | g="λg" |---|------> Lambda "g"
+        d = 3 * d;     //   | e=1    |   |
+        return d;      // ------------------------
     }
-
-    return  g();
-    var e; // variable hoisting
+                       // Local execution scope for g()  (garbage collected)
+    return  g();       // e | e=0
+    var e;             // ------------------------
 }
 
 f(1); //18
+*/
+
+// v2                   // Global Scope (Window)
+var a = 2;              // a   | a=2    |   |
+b = 1;                  // f   | f="λf" |---|------> Lambda "f"
+                        // myG |        |   |------> Lambda "g"
+function f(z) {         //     | b=1    |   | b=3    ('b' created at runtime)
+    b = 3;              //     |        |   | c=4
+    c = 4;              // ------------------------
+    var d = 6;
+    e = 1;             // Local execution scope for f() (NOT garbage collected)
+                       // z | z=1    |   |
+    function g() {     // d | d=6    |   | d=18
+        var e = 0;     // g | g="λg" |---|------> Lambda "g" is not garbage collected
+        d = 3 * d;     //   | e=1    |   |
+        return d;
+    }                  // Local execution scope for g()  (garbage collected)
+                       // e | e=0
+    return  g; // return function here
+    var e;
+}
+
+var myG = f(1); // myG is a global reference to the function Object which is a closure
+myG(); //18
+
