65cab3a19cda64cf9385e6b96a8d2ac6c07185e3
James Priest <james-priest@outlook.com>
2018-12-22 05:22:03 -0800

    fix: format code; add debug

--- javascript_exercises/fcc/js-algorithms/roman-numeral-converter.js
+++ javascript_exercises/fcc/js-algorithms/roman-numeral-converter.js
@@ -6,34 +6,35 @@
 
 function convertToRoman(num) {
   // try {
-      if (num < 1 || num > 3999) return;
-      const uni = ['I','II','III','IV','V','VI','VII','VIII','IX'],
-          dec = ['X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],
-          cent = ['C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],
-          mil = ['M','MM','MMM'];
+  console.log(`num: ${num}`);
+  if (num < 1 || num > 3999) return;
+  const uni = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+    dec = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+    cent = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+    mil = ['M', 'MM', 'MMM'];
 
-      let numArr = num.toString().split('').reverse();
-      console.log(`numArr: ${numArr.join(',')}`);
-      let newNumArr = numArr.map((val, idx) => {
-          let rn;
+  let numArr = num.toString().split('').reverse();
+  console.log(`numArr: ${numArr.join(',')}`);
+  let newNumArr = numArr.map((val, idx) => {
+    let rn;
 
-          switch (idx) {
-              case 0:
-                  rn = uni[val-1];
-                  break;
-              case 1:
-                  rn = dec[val-1];
-                  break;
-              case 2:
-                  rn = cent[val-1];
-                  break;
-              case 3:
-                  rn = mil[val-1];
-                  break;
-          }
-          console.log(`idx: ${idx}, val: ${val}, rn: ${rn}`);
-          return rn;
-       });
+    switch (idx) {
+      case 0:
+        rn = uni[val - 1];
+        break;
+      case 1:
+        rn = dec[val - 1];
+        break;
+      case 2:
+        rn = cent[val - 1];
+        break;
+      case 3:
+        rn = mil[val - 1];
+        break;
+    }
+    console.log(`idx: ${idx}, val: ${val}, rn: ${rn}`);
+    return rn;
+  });
   // }
   // catch(error) {
   //     console.log(`Caught Err - ${error}`);
