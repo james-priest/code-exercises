171965d399fe13fc4432644034c09d46664e34eb
James Priest <james-priest@outlook.com>
2018-12-22 18:11:21 -0800

    feat: Add fcc js object manipulation

---
+++ javascript_exercises/fcc/basic-js/access-object-prop-with-var.js
@@ -0,0 +1,32 @@
+var dogs = {
+  Fido: "Mutt",
+  Hunter: "Doberman",
+  Snoopie: "Beagle"
+};
+var myDog = "Hunter";
+var myBreed = dogs[myDog];
+console.log(myBreed); // "Doberman"
+
+var someObj = {
+  propName: "John"
+};
+function propPrefix(str) {
+  var s = "prop";
+  return s + str;
+}
+var someProp = propPrefix("Name"); // someProp now holds the value 'propName'
+console.log(someObj[someProp]); // "John"
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;
+var player = testObj[playerNumber];
+
+console.log(player);
