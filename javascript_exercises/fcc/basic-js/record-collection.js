7ed3defb9651ecf13fcbdefd8d65ca83f726eb43
James Priest <james-priest@outlook.com>
2019-01-01 12:33:00 -0800

    feat: Add code formatting

--- javascript_exercises/fcc/basic-js/record-collection.js
+++ javascript_exercises/fcc/basic-js/record-collection.js
@@ -17,54 +17,57 @@ If value is empty (""), delete the given prop property from the album.
  */
 // Setup
 var collection = {
-  "2548": {
-    "album": "Slippery When Wet",
-    "artist": "Bon Jovi",
-    "tracks": [ 
-      "Let It Rock", 
-      "You Give Love a Bad Name" 
-    ]
+  '2548': {
+    album: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name'],
   },
-  "2468": {
-    "album": "1999",
-    "artist": "Prince",
-    "tracks": [ 
-      "1999", 
-      "Little Red Corvette" 
-    ]
+  '2468': {
+    album: '1999',
+    artist: 'Prince',
+    tracks: [
+      '1999',
+      'Little Red Corvette',
+      'Purple Rain',
+      "Let's Go Crazy",
+      'The Beautiful Ones',
+      'Darling Nikki',
+      'Diamonds & Pearls',
+    ],
   },
-  "1245": {
-    "artist": "Robert Palmer",
-    "tracks": [ ]
+  '1245': {
+    artist: 'Robert Palmer',
+    tracks: [],
   },
-  "5439": {
-    "album": "ABBA Gold"
-  }
+  '5439': {
+    album: 'ABBA Gold',
+    test: 'Test',
+  },
 };
 // Keep a copy of the collection for tests
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
 function updateRecords(id, prop, value) {
-if(prop === 'tracks') {
-  if (!collection[id].hasOwnProperty('tracks')) {
-    var tracks = [];
-    collection[id].tracks = tracks;
-  }
-  if(value!=='') {
-    collection[id].tracks.push(value);
+  if (prop === 'tracks') {
+    if (!collection[id].hasOwnProperty('tracks')) {
+      var tracks = [];
+      collection[id].tracks = tracks;
+    }
+    if (value !== '') {
+      collection[id].tracks.push(value);
+    } else {
+      delete collection[id].tracks;
+    }
   } else {
-    delete collection[id].tracks;
+    if (value !== '') {
+      collection[id][prop] = value;
+    } else {
+      delete collection[id][prop];
+    }
   }
-} else {
-  if (value !== '') {
-    collection[id][prop] = value;
-  } else {
-    delete collection[id][prop];
-  }
-}
 
-return collection;
+  return collection;
 }
 // Alter values below to test your code
-updateRecords(5439, "artist", "ABBA");
+updateRecords(5439, 'artist', 'ABBA');
--- javascript_exercises/fcc/js-algorithms/roman-numeral-converter.js
+++ javascript_exercises/fcc/js-algorithms/roman-numeral-converter.js
@@ -5,43 +5,46 @@
  */
 
 function convertToRoman(num) {
-	// try {
-	console.log(`num: ${num}`);
-	if (num < 1 || num > 3999) return;
-	const uni = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
-		dec = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
-		cent = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
-		mil = ['M', 'MM', 'MMM'];
+  // try {
+  console.log(`num: ${num}`);
+  if (num < 1 || num > 3999) return;
+  const uni = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+    dec = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+    cent = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+    mil = ['M', 'MM', 'MMM'];
 
-	let numArr = num.toString().split('').reverse();
-	console.log(`numArr: ${numArr.join(',')}`);
-	let newNumArr = numArr.map((val, idx) => {
-		let rn;
+  let numArr = num
+    .toString()
+    .split('')
+    .reverse();
+  console.log(`numArr: ${numArr.join(',')}`);
+  let newNumArr = numArr.map((val, idx) => {
+    let rn;
 
-		switch (idx) {
-			case 0:
-				rn = uni[val - 1];
-				break;
-			case 1:
-				rn = dec[val - 1];
-				break;
-			case 2:
-				rn = cent[val - 1];
-				break;
-			case 3:
-				rn = mil[val - 1];
-				break;
-		}
-		console.log(`idx: ${idx}, val: ${val}, rn: ${rn}`);
-		return rn;
-	});
-	// }
-	// catch(error) {
-	//     console.log(`Caught Err - ${error}`);
-	// }
-	const romanNum = newNumArr.reverse().join('');
-	console.log(romanNum);
-	return romanNum;
+    switch (idx) {
+      case 0:
+        rn = uni[val - 1];
+        break;
+      case 1:
+        rn = dec[val - 1];
+        break;
+      case 2:
+        rn = cent[val - 1];
+        break;
+      case 3:
+        rn = mil[val - 1];
+        break;
+    }
+    console.log(`idx: ${idx}, val: ${val}, rn: ${rn}`);
+    return rn;
+  });
+  // }
+  // catch(error) {
+  //     console.log(`Caught Err - ${error}`);
+  // }
+  const romanNum = newNumArr.reverse().join('');
+  console.log(romanNum);
+  return romanNum;
 }
 
 convertToRoman(3999);
